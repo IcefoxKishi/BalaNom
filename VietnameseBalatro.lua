781c8a0f0394224c605e62ec02510915c3d0e694
HuyTheKiller <quochuynguyen0901@gmail.com>
2025-02-08 10:44:44 +0700

    Black UI box in description

--- VietnameseBalatro.lua
+++ VietnameseBalatro.lua
@@ -15,9 +15,14 @@ SMODS.Language({
 	warning = {"Đây là bản dịch cá nhân và được chèn vào game thông qua Steamodded.", "Nếu có thắc mắc, vui lòng liên hệ chủ bản dịch", "dưới tên theambushingbush trong máy chủ Balatro chính thức."},
 	path = "vi.lua",
 })
+
 SMODS.Atlas({
 	key = "modicon",
 	path = "icon.png",
 	px = 32,
 	py = 32
 })
+
+SMODS.current_mod.description_loc_vars = function()
+	return { background_colour = G.C.CLEAR, text_colour = G.C.WHITE, scale = 1.2 }
+end
